2ebf3ae57c823465d563d2fbe7cbec8987858fe6
Alvaro713rl <aareyeslinares@gmail.com>
2024-03-25 12:29:56 -0400

    comit desde la rama2

---
+++ app/src/main/java/com/example/terceroandroid/ActivityRama2.kt
@@ -0,0 +1,15 @@
+package com.example.terceroandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRama2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rama2)
+    }
+
+    fun funcionRama2(){
+        var fff= "la funcion desde la rama2"
+    }
+}
