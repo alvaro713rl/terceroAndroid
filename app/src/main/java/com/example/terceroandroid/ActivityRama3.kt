70ea4b527f05dd96eb687f62c8ed0f1f6e5e74cb
Alvaro713rl <aareyeslinares@gmail.com>
2024-03-25 12:33:04 -0400

    comit desde la rama3

---
+++ app/src/main/java/com/example/terceroandroid/ActivityRama3.kt
@@ -0,0 +1,15 @@
+package com.example.terceroandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRama3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rama3)
+    }
+
+    fun funcionRama3(){
+
+    }
+}
